65e1e71d789dc6f71d099b1487b4b26f2a98304b
mao <1296193245@qq.com>
2024-01-11 21:04:11 +0800

    uniapp-phoneCall

---
+++ pages/index/test1/test1.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<button type="primary" @click="call('1234567')">拨打电话1234567</button>
+		<button type='warn' @click="call('110')">拨打电话110</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			call(number)
+			{
+				uni.makePhoneCall({
+					phoneNumber: number
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
